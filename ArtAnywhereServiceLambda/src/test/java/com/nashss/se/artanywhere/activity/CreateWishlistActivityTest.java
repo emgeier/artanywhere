d583a2ae1630b865d4aabfa885b5153accfffbf1
Erin Geier <115035002+emgeier@users.noreply.github.com>
2023-05-19 15:04:38 -0500

    WishlistDao unit test saveWishlist happy case.

--- ArtAnywhereServiceLambda/src/test/java/com/nashss/se/artanywhere/activity/CreateWishlistActivityTest.java
+++ ArtAnywhereServiceLambda/src/test/java/com/nashss/se/artanywhere/activity/CreateWishlistActivityTest.java
@@ -1,33 +1,33 @@
 package com.nashss.se.artanywhere.activity;
 
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.nashss.se.artanywhere.activity.requests.CreateWishlistRequest;
 import com.nashss.se.artanywhere.activity.results.CreateWishlistResult;
 import com.nashss.se.artanywhere.dynamodb.WishlistDao;
 import com.nashss.se.artanywhere.dynamodb.models.Wishlist;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.openMocks;
 
 public class CreateWishlistActivityTest {
-@Mock
+    @Mock
     private WishlistDao wishlistDao;
-@InjectMocks
+    @InjectMocks
     private  CreateWishlistActivity createWishlistActivity;
-@BeforeEach
+    @BeforeEach
     void setup() {
-    openMocks(this);
- //   this.createWishlistActivity = new CreateWishlistActivity();
-}
-@Test
+        openMocks(this);
+    }
+    @Test
     public void handleRequest_validInput_returnsWishlist() {
-    //GIVEN
+        //GIVEN
         String expectedName = "name";
         String expectedEmail = "email";
 
@@ -40,34 +40,11 @@ public class CreateWishlistActivityTest {
         testWishlist.setListName(request.getListName());
         testWishlist.setEmail(request.getEmail());
         when(wishlistDao.saveWishlist(any())).thenReturn(testWishlist);
-    //WHEN
+        //WHEN
         CreateWishlistResult result = createWishlistActivity.handleRequest(request);
-    //THEN
-    System.out.println(result.getWishlistModel().getEmail());
+        //THEN
         assertTrue(result.getWishlistModel().getEmail().contains(request.getEmail()));
-        assertTrue(result.getWishlistModel().getListName().equals(expectedName));
-}
-//@Test
-//    public void handleRequest_inValidInput_throwsException() {
-//
-//    String expectedName = "name";
-//    String expectedEmail = "email";
-//
-//    CreateWishlistRequest request = CreateWishlistRequest.builder()
-//           // .withEmail(expectedEmail)
-//            //.withListName(expectedName)
-//            .build();
-//    Wishlist testWishlist = new Wishlist();
-//
-//    testWishlist.setListName(request.getListName());
-//    testWishlist.setEmail(request.getEmail());
-//    when(wishlistDao.saveWishlist(any())).thenReturn(testWishlist);
-//    //WHEN
-//    CreateWishlistResult result = createWishlistActivity.handleRequest(request);
-//    //THEN
-//   // System.out.println(result.getWishlistModel().getEmail());
-//    assertTrue(result.getWishlistModel().getEmail().contains(request.getEmail()));
-//   // assertTrue(result.getWishlistModel().getListName().equals(expectedName));
-//}
+        assertEquals(result.getWishlistModel().getListName(), expectedName);
+    }
 
 }
