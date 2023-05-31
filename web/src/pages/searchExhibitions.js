435f7ce2deaaf802ff47742a78115637fa27e895
Erin Geier <115035002+emgeier@users.noreply.github.com>
2023-05-30 20:32:37 -0500

    Altered design, search results a clickable list of names and institutions. When clicked will show all details, as on wishlist page.

--- web/src/pages/searchExhibitions.js
+++ web/src/pages/searchExhibitions.js
@@ -56,31 +56,66 @@ console.log(exhibitions);
     async viewSearchResults() {
 
         const exhibitions = this.dataStore.get('exhibitions');
-        console.log(exhibitions);
+
         if(exhibitions == null) {return;}
         const resultContainer = document.getElementById('view-search-results-container');
         resultContainer.classList.remove('hidden');
+
         let exhibition;
         let resultHtml = '';
         for(exhibition of exhibitions) {
+
+console.log(exhibition);
         const name = exhibition.exhibitionName;
-        console.log(name);
-            resultHtml+= `
-        <div class = "card">
-            <h4> Name: <span class="exhibition-name" id = "view-exhibition-name">${name}</span></h4>
-            <h4> Institution:   <span class="exhibition-details hidden" id = "view-institution"></span></h4>
-            <h4> Address: <span class="exhibition-details hidden" id = "view-exhibition-address"></span></h4>
-            <h4> Description:  <span class="exhibition-details hidden" id = "view-exhibition-description"></span></h4>
-            <h4> Artists: </h4>
-            <ol class = "artists" id="artists"></ol>
-            <h4> Dates:  <span class="exhibition-details hidden" id = "view-exhibition-dates"></span></h4>
-            <h4> Media: <span class="exhibition-details hidden" id = "view-exhibition-media"></span></h4>
-            <ol class = "artists" id="media"></ol>
-            <h4><span class = "exhibition-details hidden" id = "exhibition-image"></span></h4>
+//        const institution = ' ';
+//        const address = " ";
+//        const description = " ";
+
+
+        const institution = exhibition.institution;
+console.log(exhibition.institution)
+        if(institution == null){institution = "";}
+
+console.log(exhibition.address)
+
+        const address = exhibition.address;
+
+
+console.log(exhibition.description)
+
+        const description = exhibition.description;
+
+
+        resultHtml+= `
+
+
+            <a href=# <span class="exhibition-name" id = "view-exhibition-name">${name}   :::   ${institution}  </span></a>
+            <br>
             <br>
         `;
         }
         document.getElementById('exhibitions').innerHTML = resultHtml;
+
+    }
+    async createExhibitionDetailsCard(exhibition) {
+//                <h4> Institution:   <span id = "view-institution">${institution}</span></h4>
+//                <h4> Address: <span class="exhibition-details" id = "view-exhibition-address">${address}</span></h4>
+//                <h4> Description:  <span class="exhibition-details " id = "view-exhibition-description">${description}</span></h4>
+//                <h4> Dates:  <span class="exhibition-details " id = "view-exhibition-dates"></span></h4>
+//                <h4> Media: <span class="exhibition-details " id = "view-exhibition-media"></span></h4>
+//          <ol class = "artists" id="media"></ol>
+//                <h4><span class = "exhibition-details " id = "exhibition-image"></span></h4>
+//                if(exhibition.imageUrl != null ){
+//                    const imageUrl = exhibition.imageUrl;
+//                    const urlAttribution = exhibition.imageAttribution;
+//                    let urlHtml = `<img src=${imageUrl} alt="Image description" width="500" height="300"> <br>
+//                <span id = "attribution" >${urlAttribution}</span>
+//            `;
+//
+//        document.getElementById("exhibition-image").innerHTML =
+//            urlHtml;
+//
+//                }
     }
 }
 /**
