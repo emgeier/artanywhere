7c36779fb77da64a9761d899ba260ba1ccb2b1c4
Erin Geier <115035002+emgeier@users.noreply.github.com>
2023-06-20 22:21:40 -0500

    started fixing checkstyle errors

--- ArtAnywhereServiceLambda/src/main/java/com/nashss/se/artanywhere/lambda/LambdaRequest.java
+++ ArtAnywhereServiceLambda/src/main/java/com/nashss/se/artanywhere/lambda/LambdaRequest.java
@@ -25,9 +25,7 @@ public class LambdaRequest<T> extends APIGatewayProxyRequestEvent {
      */
     public T fromBody(Class<T> requestClass) {
         log.info("fromBody");
-System.out.println("fromBody");
         try {
-System.out.println(super.getBody());
             return MAPPER.readValue(super.getBody(), requestClass);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(
@@ -35,17 +33,6 @@ System.out.println(super.getBody());
                     e);
         }
     }
-
-    /**
-     * Use the given converter to create an instance of T from the request's query string.
-     * @param converter Contains the conversion code
-     * @return An instance of T that contains data from the request's query string
-     */
-//    public T fromQuery(Function<Map<String, String>, T> converter) {
-//        Map<String, String> query = ifNull(super.getQueryStringParameters(), Map.of());
-//        return converter.apply(query);
-//    }
-
     /**
      * Use the given converter to create an instance of T from the request's path parameters.
      * @param converter Contains the conversion code
@@ -53,24 +40,11 @@ System.out.println(super.getBody());
      */
     public T fromPath(Function<Map<String, String>, T> converter) {
         log.info("fromPath");
-System.out.println("fromPath");
-System.out.println(super.getPathParameters());
+
         Map<String, String> path = ifNull(super.getPathParameters(), Map.of());
         return converter.apply(path);
     }
 
-    /**
-     * Use the given converter to create an instance of T from the request's path parameters
-     * and query string parameters.
-     * @param converter Contains the conversion code
-     * @return A instance of T that contains data from the request's path parameters
-     */
-//    public T fromPathAndQuery(BiFunction<Map<String, String>, Map<String, String>, T> converter) {
-//        log.info("fromPathAndQuery");
-//        Map<String, String> path = ifNull(super.getPathParameters(), Map.of());
-//        Map<String, String> query = ifNull(super.getQueryStringParameters(), Map.of());
-//        return converter.apply(path, query);
-//    }
     /**
      * If obj is null, return valIfNull, otherwise return obj.
      * @param obj The object to check for null.
@@ -82,15 +56,5 @@ System.out.println(super.getPathParameters());
         return obj != null ? obj : valIfNull;
     }
 
-//    /**
-//     * If obj is null, return value supplied by valIfNullSupplier.
-//     * @param obj The object to check for null.
-//     * @param valIfNullSupplier The supplier of the value to return if obj is null.
-//     * @param <T> The type of obj and the supplier.
-//     * @return obj or value returned by valIfNullSupplier.
-//     */
-//    public static <T> T ifNull(T obj, Supplier<T> valIfNullSupplier) {
-//        return obj != null ? obj : valIfNullSupplier.get();
-//    }
 }
 
